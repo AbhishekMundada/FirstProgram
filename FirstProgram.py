6414ca3543cd3802ecf89b37e12eb90c79f4f3a8
Abhishek Mundada <abhi.mundada29@gmail.com>
2022-05-02 15:02:22 +0530

    Added FirstProgram of python

---
+++ FirstProgram.py
@@ -0,0 +1,9 @@
+#print("Enter your first number")
+#n1=input()
+
+#print("Enter your second number")
+#n2=input()
+
+#print("Sum of two numbers : ", int(n1)+int(n2))
+
+print("Sum of two numbers : ", 5+6)
